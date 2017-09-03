e1c4259de4e93aec87dc4054855c4ecbc86127c1
duanfa <duanfa1987@gmail.com>
2017-09-03 20:47:18 +0800

    create

--- build.sbt
+++ build.sbt
@@ -1,6 +1,68 @@
-lazy val root = (project in file(".")).  
-    settings(
-    name := "sparkMl",
-    version := "1.0",
-    scalaVersion := "2.11.7"
+
+import AssemblyKeys._
+
+assemblySettings
+
+
+
+name := "sparkML"
+
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+javacOptions ++= Seq("-source", "1.7", "-target", "1.7")
+resolvers += "Typesafe Repo" at "http://repo.typesafe.com/typesafe/releases/"
+
+libraryDependencies ++= {
+
+  val sparkVersion = "2.0.1"
+  val hadoopVersion = "2.4.0"
+  Seq(
+    "org.apache.spark" %% "spark-core" % sparkVersion ,
+    "org.apache.spark" %% "spark-sql" % sparkVersion ,
+    "org.apache.spark" %% "spark-mllib" % sparkVersion ,
+    //"org.apache.spark" %% "spark-streaming" % sparkVersion ,
+   // "org.apache.spark" % "spark-streaming-kafka_2.10" % sparkVersion,
+    // "org.scala-lang" % "scala-compiler" % "2.10.6" ,
+    //"org.scala-lang" % "scala-reflect" % "2.10.6" ,
+    "org.apache.spark" %% "spark-graphx" % sparkVersion ,
+    "org.apache.spark" %% "spark-catalyst" % sparkVersion,
+    "org.apache.hadoop" % "hadoop-client" % hadoopVersion
+
+    /* "org.apache.phoenix" % "phoenix-spark" % "4.6.0-HBase-0.98",
+    "org.apache.phoenix" % "phoenix-core" % "4.6.0-HBase-0.98",
+    "org.apache.hbase" % "hbase-client" % "0.98.12-hadoop2",
+    "org.apache.hbase" % "hbase-common" % "0.98.12-hadoop2",
+    "org.apache.hbase" % "hbase-server" % "0.98.12-hadoop2",
+   "org.apache.hbase" % "hbase-protocol" % "0.98.12-hadoop2"*/
   )
+    .map(_ % "provided")
+    .map(_.excludeAll(ExclusionRule(organization = "org.mortbay.jetty")))
+}
+
+libraryDependencies ++={
+  Seq(
+    "redis.clients" % "jedis" % "2.8.1",
+    "com.huaban"%"jieba-analysis"%"1.0.2",
+    "com.github.nscala-time" %% "nscala-time" % "2.2.0",
+    "mysql" % "mysql-connector-java" % "5.1.34"  ,
+    //    "org.scalatest" % "scalatest_2.10" % "2.0" % "test",
+    "com.alibaba" % "fastjson" % "1.1.21",
+    "com.github.scopt" %% "scopt" % "3.3.0",
+    "sqlline" % "sqlline" % "1.1.9",
+    "org.jblas" % "jblas" % "1.2.4",
+    
+    //"net.sf.ezmorph" % "ezmorph" % "1.0.6",
+    "net.sf.json-lib" % "json-lib" % "1.0" classifier "jdk15",
+    "org.scalaj" % "scalaj-http_2.11" % "2.3.0"
+  ).map(
+    _.excludeAll(ExclusionRule(organization = "org.mortbay.jetty"))
+  )
+
+}
+
+
+
+resolvers += Resolver.sonatypeRepo("public")
+jarName in assembly := "UFSRepice.jar"
---
+++ src/main/scala/com/ufs/cluster/localTest/Scalertest.scala
@@ -0,0 +1,119 @@
+package com.ufs.cluster.localTest
+
+import scala.reflect.runtime.universe
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+import org.apache.spark.ml.feature.IDF
+import org.apache.spark.ml.feature.MinMaxScaler
+import org.apache.spark.ml.feature.Normalizer
+import org.apache.spark.ml.feature.StandardScaler
+import org.apache.spark.ml.feature.VectorIndexer
+import org.apache.spark.ml.linalg.SparseVector
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.feature.MaxAbsScaler
+
+/**
+ * @author socket
+ */
+object Scalertest {
+  def main(args: Array[String]) {
+    println("useage: args(0): inputPath  args(0):recipTopN")
+    val conf = new SparkConf().setAppName("RestaurantCluster").setMaster("local[2]")
+    val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
+    val array = Array[(Double, org.apache.spark.ml.linalg.Vector)](
+      (1.0 -> Vectors.dense(1, 2, 3, 4)),
+      (2.0 -> Vectors.dense(2, 4, 6, 8)),
+      (3.0 -> Vectors.dense(9, 10, 11, 12)),
+      (4.0 -> Vectors.dense(13, 14, 15, 16)),
+      (5.0 -> Vectors.dense(18, 36, 54, 72)))
+
+    val dataFrame = sqlContext.createDataFrame(array)
+    dataFrame.printSchema()
+    dataFrame.show()
+    var after = Maxscaler(dataFrame)
+    println("--------------Maxscaler------------------")
+    after.map(f =>{ 
+      println(f._1,f._2)
+      1
+    }).reduce(_+_)
+    
+    after = TFidfScaler(dataFrame)
+    println("--------------TFidfScaler------------------")
+    after.map(f =>{ 
+      println(f._1,f._2)
+      1
+    }).reduce(_+_)
+  }
+
+  def Maxscaler(dataFrame: org.apache.spark.sql.DataFrame): RDD[(Double, org.apache.spark.ml.linalg.Vector)] = {
+    val data = dataFrame.toDF("id", "features")
+    val scaler = new MaxAbsScaler()
+      .setInputCol("features")
+      .setOutputCol("scaledFeatures")
+    val scalerModel = scaler.fit(data)
+    val scaledData = scalerModel.transform(data)
+    scaledData.select("id", "scaledFeatures").rdd.map { x => x.getAs[Double](0) -> Vectors.dense(x.getAs[SparseVector](1).toDense.values) }
+  }
+
+  def VectorIndexer(dataFrame: org.apache.spark.sql.DataFrame): RDD[(Double, org.apache.spark.ml.linalg.Vector)] = {
+    val data = dataFrame.toDF("id", "features")
+    val indexer = new VectorIndexer()
+      .setInputCol("features")
+      .setOutputCol("scaledFeatures")
+      .setMaxCategories(10);
+    val indexerModel = indexer.fit(data);
+    val scaledFeatures = indexerModel.transform(data)
+    scaledFeatures.select("id", "scaledFeatures").rdd.map { x => x.getAs[Double](0) -> Vectors.dense(x.getAs[SparseVector](1).toDense.values) }
+
+  }
+
+  def normalizer(dataFrame: org.apache.spark.sql.DataFrame): RDD[(Double, org.apache.spark.ml.linalg.Vector)] = {
+    val data = dataFrame.toDF("id", "features")
+    val normalizer = new Normalizer()
+      .setInputCol("features")
+      .setOutputCol("scaledFeatures")
+      .setP(1.0)
+    val scaledFeatures = normalizer.transform(data)
+    scaledFeatures.select("id", "scaledFeatures").rdd.map { x => x.getAs[Double](0) -> Vectors.dense(x.getAs[SparseVector](1).toDense.values) }
+
+  }
+
+  def StandardScaler(dataFrame: org.apache.spark.sql.DataFrame): RDD[(Double, org.apache.spark.ml.linalg.Vector)] = {
+    val data = dataFrame.toDF("id", "features")
+    val scaler = new StandardScaler()
+      .setInputCol("features")
+      .setOutputCol("scaledFeatures")
+      .setWithStd(true)
+      .setWithMean(false)
+    val indexerModel = scaler.fit(data);
+    val scaledFeatures = indexerModel.transform(data)
+    scaledFeatures.select("id", "scaledFeatures").rdd.map { x => x.getAs[Double](0) -> Vectors.dense(x.getAs[SparseVector](1).toDense.values) }
+
+  }
+
+  def MinMaxScaler(dataFrame: org.apache.spark.sql.DataFrame): RDD[(Double, org.apache.spark.ml.linalg.Vector)] = {
+    val data = dataFrame.toDF("id", "features")
+    val scaler = new MinMaxScaler()
+      .setInputCol("features")
+      .setOutputCol("scaledFeatures")
+    val indexerModel = scaler.fit(data);
+    val scaledFeatures = indexerModel.transform(data)
+    scaledFeatures.select("id", "scaledFeatures").rdd.map { x => x.getAs[Double](0) -> Vectors.dense(x.getAs[SparseVector](1).toDense.values) }
+
+  }
+
+  def TFidfScaler(dataFrame: org.apache.spark.sql.DataFrame): RDD[(Double, org.apache.spark.ml.linalg.Vector)] = {
+    val data = dataFrame.toDF("id", "features")
+    val idf = new IDF()
+      .setInputCol("features")
+      .setOutputCol("scaledFeatures")
+    val indexerModel = idf.fit(data);
+    val scaledFeatures = indexerModel.transform(data)
+    scaledFeatures.select("id", "scaledFeatures").rdd.map { x => x.getAs[Double](0) -> Vectors.dense(x.getAs[SparseVector](1).toDense.values) }
+
+  }
+}
