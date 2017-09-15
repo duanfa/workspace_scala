277944c4c2b05ad418d054fd74fb8db1e9e2b80e
duanfa <385104182@qq.com>
2017-09-15 17:27:03 +0800

    add

--- src/main/scala/com/st/math/linalg/Eigenvalues.scala
+++ src/main/scala/com/st/math/linalg/Eigenvalues.scala
@@ -1,7 +1,11 @@
 package com.st.math.linalg;
 
-import breeze.linalg._
+import breeze.linalg.DenseMatrix
+import breeze.linalg.Vector
+import breeze.linalg.eigSym
 import breeze.linalg.eigSym.EigSym
+import breeze.linalg.DenseVector
+//import org.apache.spark.mllib.linalg.Vector
 /*
  * 求特征值和特征向量
  */
@@ -10,13 +14,19 @@ object Eigenvalues {
 
   def main(args: Array[String]) {
 
-    val A = DenseMatrix((0.6165555555555555, 0.6154444444444445),
-      (0.6154444444444445, 0.7165555555555555))
+    val A = DenseMatrix((3.0, 6.0, 7.0),
+      (6.0, 3.0, 9.0),
+      (7.0, 9.0, 1.0))
     val EigSym(lambda, evs) = eigSym(A)
     val es = eigSym(A)
     println(es.eigenvalues)
-    println(es.eigenvectors)
-
+    val eigenvectors = es.eigenvectors
+    println("-----------------------------------")
+    println(eigenvectors)
+    println("-----------------------------------")
+    println(eigenvectors(0,0)*eigenvectors(0,1)+eigenvectors(1,0)*eigenvectors(1,1)+eigenvectors(2,0)*eigenvectors(2,1))
+    println(eigenvectors(0,2)*eigenvectors(0,1)+eigenvectors(1,2)*eigenvectors(1,1)+eigenvectors(2,2)*eigenvectors(2,1))
+    println(eigenvectors(0,0)*eigenvectors(0,2)+eigenvectors(1,0)*eigenvectors(1,2)+eigenvectors(2,0)*eigenvectors(2,2))
   }
 }
 
