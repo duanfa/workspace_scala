44af97e722e0970cae622b21fda594702d93ab70
duanfa <385104182@qq.com>
2017-09-15 21:32:49 +0800

    add

---
+++ src/main/scala/com/st/decomposition/LDA/Snippet.java
@@ -0,0 +1,9 @@
+package com.st.decomposition.LDA;
+
+public class Snippet {
+	public static void main(String[] args) {
+//		Linear Discriminant Analysis (也有叫做Fisher Linear Discriminant)是一种有监督的（supervised）线性降维算法。与PCA保持数据信息不同，LDA是为了使得降维后的数据点尽可能地容易被区分！
+//		http://dataunion.org/13451.html
+	}
+}
+
